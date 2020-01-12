2412a76c565d9c2fa4cf80a1a54e8c820083d9d3
Emma Rafkin <emmarafkin@gmail.com>
2020-01-12 15:21:09 -0800

    added resume

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,17 @@
 import React, {Component} from 'react';
+import doc from '../assets/document.png';
 import github from '../assets/github.png';
 import email from '../assets/email.png';
 import linkedIn from '../assets/linkedin.png';
 
 class Header extends Component{
+
     render(){
         return(
             <div className = "links">
-            <a href = "https://github.com/erafkin/" target = "_blank" rel = "noopener noreferrer"><img src = {github}/> </a>
-              <a href = "https://www.linkedin.com/in/emma-rafkin-2966406b/" target = "_blank" rel = "noopener noreferrer"><img src = {linkedIn}/> </a> 
+                <a href = "http://tiny.cc/4m7liz" target = "_blank" rel = "noopener noreferrer"><img src = {doc}/> </a>
+                <a href = "https://github.com/erafkin/" target = "_blank" rel = "noopener noreferrer"><img src = {github}/> </a>
+                <a href = "https://www.linkedin.com/in/emma-rafkin-2966406b/" target = "_blank" rel = "noopener noreferrer"><img src = {linkedIn}/> </a> 
                 <a href = "mailto:emma.p.rafkin.21@dartmouth.edu"><img src = {email}/> </a>
            </div>
 
@@ -16,3 +19,4 @@ class Header extends Component{
     }
 
 }export default Header;
+
--- src/components/touch.js
+++ src/components/touch.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import github from '../assets/github.png';
 import email from '../assets/email.png';
 import linkedIn from '../assets/linkedin.png';
+import doc from '../assets/document.png'
 
 
 class Touch extends Component {
@@ -15,9 +16,10 @@ class Touch extends Component {
         </div>
         <br/>
         <div>
+            <a href = "http://tiny.cc/4m7liz" target = "_blank" rel = "noopener noreferrer"><img src = {doc}/> </a>
             <a href = "https://github.com/erafkin/" target = "_blank" rel = "noopener noreferrer"><img src = {github}/> </a>
-              <a href = "https://www.linkedin.com/in/emma-rafkin-2966406b/" target = "_blank" rel = "noopener noreferrer"><img src = {linkedIn}/> </a> 
-                <a href = "mailto:emma.p.rafkin.21@dartmouth.edu"><img src = {email}/> </a>
+            <a href = "https://www.linkedin.com/in/emma-rafkin-2966406b/" target = "_blank" rel = "noopener noreferrer"><img src = {linkedIn}/> </a> 
+            <a href = "mailto:emma.p.rafkin.21@dartmouth.edu"><img src = {email}/> </a>
         </div>
 
    
