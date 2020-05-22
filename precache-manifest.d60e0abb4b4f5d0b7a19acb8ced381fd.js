4d82bc7dfab07759bc10b33087cc88c647773771
Emma Rafkin <emmarafkin@gmail.com>
2020-05-22 14:52:37 -0400

    Updates

---
+++ precache-manifest.d60e0abb4b4f5d0b7a19acb8ced381fd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9770200edec341b8e7b402e2deff6eff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68f4ae0cb2f16d6284c1",
+    "url": "/static/css/main.7ecb9c84.chunk.css"
+  },
+  {
+    "revision": "8b5465293d7d9690186d",
+    "url": "/static/js/2.08037754.chunk.js"
+  },
+  {
+    "revision": "68f4ae0cb2f16d6284c1",
+    "url": "/static/js/main.59399a5e.chunk.js"
+  },
+  {
+    "revision": "ec9c4b3a47b00f6dda41",
+    "url": "/static/js/runtime~main.3dc7c543.js"
+  },
+  {
+    "revision": "6fd9df819d4ebf7334eb4daedfb10ea1",
+    "url": "/static/media/doc.6fd9df81.png"
+  },
+  {
+    "revision": "aa16c7a820c371d39a1d2894c9cb0ec6",
+    "url": "/static/media/driven.aa16c7a8.png"
+  },
+  {
+    "revision": "6ac311cc9c4af6e6955843e4f1036744",
+    "url": "/static/media/lc19x.6ac311cc.png"
+  },
+  {
+    "revision": "f7275592cda8778827041416fdf864e0",
+    "url": "/static/media/me.f7275592.jpg"
+  }
+]);
