f823f706f93e639366bbd0f05b68ba20ce45d423
Emma Rafkin <emmarafkin@gmail.com>
2021-04-20 11:27:04 -0400

    new resume

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,7 @@ class Header extends Component{
     render(){
         return(
             <div className = "links">
-                <a href = "https://tinyurl.com/y6m9528k" target = "_blank" rel = "noopener noreferrer" ><img src = {doc} alt = "doc icon"/></a>
+                <a href = "https://tinyurl.com/vxp4zdwv" target = "_blank" rel = "noopener noreferrer" ><img src = {doc} alt = "doc icon"/></a>
                 <a href = "https://github.com/erafkin/" target = "_blank" rel = "noopener noreferrer"><img src = {github} alt = "github icon"/></a>
                 <a href = "https://www.linkedin.com/in/emma-rafkin-2966406b/" target = "_blank" rel = "noopener noreferrer"><img src = {linkedIn} alt = "linkedIn icon"/></a> 
                 <a href = "mailto:emmarafkin@gmail.com"><img src = {email} alt = "email icon"/></a>
